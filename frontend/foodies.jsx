1423356e646d3b48351b4358e9cd7e3d81d91603
Brandon Tat <brandontat7@gmail.com>
2017-05-16 15:23:48 -0700

    Bootstrap user

--- frontend/foodies.jsx
+++ frontend/foodies.jsx
@@ -7,9 +7,16 @@ import Root from './components/root';
 import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
   const root = document.getElementById('root');
+  let store;
 
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser }};
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   //REMOVE AFTER TESTING
   window.store = store;
   window.signup = signup;
