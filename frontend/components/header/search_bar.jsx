54b686061fab062c8d6abe02f171bbdf41bfe617
Brandon Tat <brandontat7@gmail.com>
2017-05-19 09:56:21 -0700

    Update search bar

--- frontend/components/header/search_bar.jsx
+++ frontend/components/header/search_bar.jsx
@@ -4,32 +4,38 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      option: "Test"
+      option: ""
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.change = this.change.bind(this);
+    // this.change = this.change.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  change(e) {
-    e.preventDefault();
+  // change(e) {
+  //   e.preventDefault();
+  //   this.setState({ option: e.target.value });
+  // }
+
+  handleChange(e) {
     this.setState({ option: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    console.log(this.state.option);
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <select onChange={this.change}
-          value={this.state.option}
-          id="search">
-          <option value="A">Apple</option>
-          <option value="B">Banana</option>
-          <option value="C">Cranberry</option>
-        </select>
+        <label>Find:</label>
+        <input
+          value = {this.state.option}
+          placeholder = "Tacos, burgers, etc."
+          onChange = {this.handleChange}
+        />
+
         <button><i className="fa fa-search"></i></button>
       </form>
   );
@@ -37,3 +43,17 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+
+
+
+// <form onSubmit={this.handleSubmit}>
+//   <select onChange={this.change}
+//     value={this.state.option}
+//     id="search">
+//     <option value="A">Apple</option>
+//     <option value="B">Banana</option>
+//     <option value="C">Cranberry</option>
+//   </select>
+//   <button><i className="fa fa-search"></i></button>
+// </form>
