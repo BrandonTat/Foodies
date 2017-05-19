c40634b46a674f764b7555bcef5c88289e96990e
Brandon Tat <brandontat7@gmail.com>
2017-05-19 10:49:29 -0700

    Update businesses routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-    resources :business
+    resources :businesses
   end
   root "static_pages#root"
 end
