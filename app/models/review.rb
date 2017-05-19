e6b2566b4266747d0b3cac6f748bfc7d1c43af0d
Brandon Tat <brandontat7@gmail.com>
2017-05-19 10:11:23 -0700

    Migrate reviews table

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id          :integer          not null, primary key
+#  business_id :integer          not null
+#  user_id     :integer          not null
+#  review_text :text             not null
+#  rating      :integer          not null
+#  price       :integer          not null
+#  photo_url   :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Review < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517222303) do
+ActiveRecord::Schema.define(version: 20170519170021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,19 @@ ActiveRecord::Schema.define(version: 20170517222303) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_businesses_on_category_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "business_id", null: false
+    t.integer  "user_id",     null: false
+    t.text     "review_text", null: false
+    t.integer  "rating",      null: false
+    t.integer  "price",       null: false
+    t.string   "photo_url",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["business_id"], name: "index_reviews_on_business_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
