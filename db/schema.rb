fb53242ba499fc454b26ae2fc0bf3bccce1da5a7
Brandon Tat <brandontat7@gmail.com>
2017-05-19 10:12:45 -0700

    Add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519170021) do
+ActiveRecord::Schema.define(version: 20170519171154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20170519170021) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_businesses_on_category_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "type",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
