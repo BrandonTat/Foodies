dc3af40c70ee0cbc294d9a80523c0bf9ebb8f881
Brandon Tat <brandontat7@gmail.com>
2017-05-21 00:37:55 -0700

    Log business id after clicking on business name

--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 class BusinessIndexItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleBusinessClick = this.handleBusinessClick.bind(this);
+  }
+
+  handleBusinessClick(e) {
+    e.preventDefault;
+    console.log(this.props.business.id);
   }
 
   render() {
     const { name, price, address } = this.props.business;
     return (
-      <ul>
+      <ul id="business" onClick= {this.handleBusinessClick}>
         <li>{name}</li>
+        <li>{address}</li>
       </ul>
     );
   }
