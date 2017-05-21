d85f5d338807812a403902548a567531c673c8f0
Brandon Tat <brandontat7@gmail.com>
2017-05-21 16:27:58 -0700

    Add cloudinary photos

--- frontend/components/home/home_index.jsx
+++ frontend/components/home/home_index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const p1 = "https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg";
-const p2 = "https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb";
-const p3 = "https://images.pexels.com/photos/29302/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb";
-const p4 = "https://images.pexels.com/photos/245513/pexels-photo-245513.jpeg?h=350&auto=compress&cs=tinysrgb";
-const p5 = "https://images.pexels.com/photos/24565/pexels-photo-24565.jpg?h=350&auto=compress&cs=tinysrgb";
-const p6 = "https://images.pexels.com/photos/8374/food-ice-cream.jpg?h=350&auto=compress&cs=tinysrgb";
-const p7 = "https://images.pexels.com/photos/41967/appetizer-canape-canapes-cheese-41967.jpeg?h=350&auto=compress&cs=tinysrgb";
-const p8 = "https://images.pexels.com/photos/47483/pexels-photo-47483.jpeg?h=350&auto=compress&cs=tinysrgb";
-const p9 = "https://images.pexels.com/photos/4863/restaurant-lighting-lobster-neon.jpg?h=350&auto=compress&cs=tinysrgb";
+const p2 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407315/brunch_v0cewv.jpg";
+const p3 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/clams_xp3lyc.jpg";
+const p4 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/food-ice-cream_wvsst4.jpg";
+const p5 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/grilled_chicken_hh1v42.jpg";
+const p6 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/burgers_myej8b.jpg";
+const p7 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/crackers_i8cweo.jpg";
+const p8 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/acai_x5wveh.jpg";
+const p9 = "http://res.cloudinary.com/dkdpoayma/image/upload/v1495407314/lobster_s9v3we.jpg";
 
 const PHOTOS = [p1, p2, p3, p4, p5, p6, p7, p8, p9];
 
