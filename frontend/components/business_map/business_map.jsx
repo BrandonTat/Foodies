939f0a18aa25058e2ab060bc8261736de8269eea
Brandon Tat <brandontat7@gmail.com>
2017-05-21 00:23:49 -0700

    Display business information when hovering over map marker

--- frontend/components/business_map/business_map.jsx
+++ frontend/components/business_map/business_map.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MarkerManager from '../../util/marker_manager';
+import { values } from 'lodash';
 
 const mapOptions = {
   center: { lat: 37.7758, lng: -122.435},
@@ -11,10 +12,36 @@ class BusinessMap extends React.Component{
     this.map = new google.maps.Map(this.refs.map, mapOptions);
     this.MarkerManager = new MarkerManager(this.map);
     this.MarkerManager.updateMarkers(this.props.businesses);
+
+    this.showBusiness = this.showBusiness.bind(this);
   }
 
   componentDidUpdate() {
     this.MarkerManager.updateMarkers(this.props.businesses);
+
+    const markers = values(this.MarkerManager.markers);
+
+    markers.forEach(marker => {
+      this.showBusiness(marker);
+    });
+  }
+
+  showBusiness(marker) {
+    let businessId = marker.businessId;
+    let businessName = this.props.businesses[businessId].name;
+
+    const window = new google.maps.InfoWindow({
+      content: businessName,
+      maxWidth: 200
+    });
+
+    marker.addListener('mouseover', () => {
+      window.open(this.map, marker);
+    });
+
+    marker.addListener('mouseout', () => {
+      window.close(this.map, marker);
+    });
   }
 
   render() {
