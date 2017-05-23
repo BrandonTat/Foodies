a7477d1d9ec52c6606598b358ca1c999c4a39db2
Brandon Tat <brandontat7@gmail.com>
2017-05-22 23:06:42 -0700

    Add fetchBusiness to util

--- frontend/util/business_api_util.js
+++ frontend/util/business_api_util.js
@@ -1,3 +1,10 @@
+export const fetchBusiness = id => {
+  $.ajax({
+    method: "GET",
+    url: `api/businesses/${id}`
+  });
+};
+
 export const fetchBusinesses = () => {
   return $.ajax({
     method: "GET",
