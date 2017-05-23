cbf534957683f510beb490d9c589c3242697dd10
Brandon Tat <brandontat7@gmail.com>
2017-05-22 23:04:00 -0700

    Add receiveBusiness to business actions

--- frontend/actions/business_actions.js
+++ frontend/actions/business_actions.js
@@ -1,11 +1,23 @@
 import * as APIUtil from '../util/business_api_util';
 
+export const RECEIVE_BUSINESS = "RECEIVE_BUSINESS";
 export const RECEIVE_BUSINESSES = "RECEIVE_BUSINESSES";
+
+export const receiveBusiness = business => ({
+  type: RECEIVE_BUSINESS,
+  business
+});
 
 export const receiveBusinesses = businesses => ({
   type: RECEIVE_BUSINESSES,
   businesses
 });
+
+export const fetchBusiness = id => dispatch => (
+  APIUtil.fetchBusiness(id).then(business => (
+    dispatch(receiveBusiness)
+  ))
+);
 
 export const fetchBusinesses = () => dispatch => (
   APIUtil.fetchBusinesses().then(businesses => (
--- frontend/components/business_show/business_show_container.js
+++ frontend/components/business_show/business_show_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 
 import BusinessShow from './business_show';
+import { fetchBusiness } from '../actions/business_actions';
 
 const mapStateToProps = (state, { match }) => ({
   businessId: parseInt(match.params.businessId)
@@ -14,3 +15,27 @@ export default connect(
   mapStateToProps,
   null
 )(BusinessShow);
+
+// import { connect } from 'react-redux';
+//
+// import { fetchBench } from '../../actions/bench_actions';
+// import { selectBench } from '../../reducers/selectors';
+// import BenchShow from './bench_show';
+//
+// const mapStateToProps = (state, { match }) => {
+//   const benchId = parseInt(match.params.benchId);
+//   const bench = selectBench(state, match.params.benchId);
+//   return {
+//     benchId,
+//     bench
+//   };
+// };
+//
+// const mapDispatchToProps = dispatch => ({
+//   fetchBench: id => dispatch(fetchBench(id))
+// });
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(BenchShow);
