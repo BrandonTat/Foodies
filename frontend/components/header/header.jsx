f9c7e91c6512d50114b8d6f8976f0c6ccc26e170
BrandonTat <brandontat7@gmail.com>
2018-11-12 22:23:59 -0800

    Update CSS for header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -23,19 +23,22 @@ class Header extends React.Component {
     if (!this.props.currentUser) {
       return this.redirectLogin();
     }
+
     return (
-      <div id="header">
-        <div id="headerLinks">
-          <Link className="link" id="homeLink" to="/home">Foodies</Link>
-          <a id="github" href="https://github.com/BrandonTat/yelp_clone">
+      <div className="header">
+        <div className="headerLinks">
+          <Link className="link" to="/home">Foodies</Link>
+          <a href="https://github.com/BrandonTat/yelp_clone">
             <i className="fa fa-github link" aria-hidden="true"></i>
           </a>
           <a href="https://www.linkedin.com/in/brandon-tat-300815120/">
             <i className="fa fa-linkedin link" aria-hidden="true"></i>
           </a>
         </div>
+
         <SearchBar {...this.props}/>
-      <button className="link" onClick={this.handleLogout}>Log Out</button>
+
+        <button className="logout-link" onClick={this.handleLogout}>Log Out</button>
       </div>
     );
   }
